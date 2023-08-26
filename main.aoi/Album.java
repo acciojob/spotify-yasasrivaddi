cb9bab75a296652cf64fa7fa5350e57dc0820093
yasasrivaddi <136231520+yasasrivaddi@users.noreply.github.com>
2023-08-27 03:23:47 +0530

    Create Album.java

---
+++ main.aoi/Album.java
@@ -0,0 +1,11 @@
+public class Album {
+    private String title;
+    private String artistName;
+
+    public Album(String title, String artistName) {
+        this.title = title;
+        this.artistName = artistName;
+    }
+
+    // Getters and setters
+}
