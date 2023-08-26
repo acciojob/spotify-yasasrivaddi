927faee0c5dd7fa143c7391abd9898e29ea6283a
yasasrivaddi <136231520+yasasrivaddi@users.noreply.github.com>
2023-08-27 03:22:55 +0530

    Create User.java

---
+++ main.aoi/User.java
@@ -0,0 +1,11 @@
+public class User {
+    private String name;
+    private String mobileNumber;
+
+    public User(String name, String mobileNumber) {
+        this.name = name;
+        this.mobileNumber = mobileNumber;
+    }
+
+    // Getters and setters
+}
