abb44c0cda728780e2d0278ab8b49fb1a7d63afc
yasasrivaddi <136231520+yasasrivaddi@users.noreply.github.com>
2023-08-27 03:23:27 +0530

    Create Artist.java

---
+++ main.aoi/Artist.java
@@ -0,0 +1,11 @@
+public class Artist {
+    private String name;
+    private int likes;
+
+    public Artist(String name) {
+        this.name = name;
+        this.likes = 0;
+    }
+
+    // Getters and setters
+}
