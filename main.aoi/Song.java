4cf702b77fb3fa2cde73ee5416ddbe20ad2d7c38
yasasrivaddi <136231520+yasasrivaddi@users.noreply.github.com>
2023-08-27 03:24:06 +0530

    Create Song.java

---
+++ main.aoi/Song.java
@@ -0,0 +1,13 @@
+public class Song {
+    private String title;
+    private String albumName;
+    private int likes;
+
+    public Song(String title, String albumName) {
+        this.title = title;
+        this.albumName = albumName;
+        this.likes = 0;
+    }
+
+    // Getters and setters
+}
